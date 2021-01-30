1177598e26572e5285192b1097c43a4c65b920ee
aurudin <renon@renon.com.br>
2021-01-30 15:42:11 -0300

    Add Page Cursos, Quem Somos

---
+++ page-depoimentos.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package academiadaodontologia
+ */
+
+get_header();
+?>
+
+<main id="main primary" class="site-main">
+  <section class="mt-5 py-5">
+    <div class="container mt-5 mb-5">
+      <div class="row">
+        <div class="col">
+          <div class="ui-title">
+            <h2 class="ui-title-main">O que dizem nossos alunos</h2>
+            <!-- <span class="ui-title-sub"> some subtitle</span> -->
+          </div>
+        </div>
+      </div>
+      <div class="row row-cols-1 row-cols-md-2 g-5" data-masonry='{"percentPosition": true }'>
+        <div class="col">
+          <div class="card card-testimonial" data-aos="fade-up">
+            <div class="card-testimonial__media">
+              <div class="videoWrapper">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/Do5HAPmcL2c" frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen></iframe>
+              </div>
+            </div>
+            <h3 class="card-testimonial__name">Testimonial Name</h3>
+            <span class="card-testimonial__position">Testimonial position</span>
+          </div>
+        </div> <!-- //col-->
+        <div class="col">
+          <div class="card card-testimonial" data-aos="fade-up">
+            <div class="card-testimonial__media">
+              <div class="videoWrapper">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/Do5HAPmcL2c" frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen></iframe>
+              </div>
+            </div>
+            <h3 class="card-testimonial__name">Testimonial Name</h3>
+            <span class="card-testimonial__position">Testimonial position</span>
+          </div>
+        </div> <!-- //col-->
+        <div class="col">
+          <div class="card card-testimonial" data-aos="fade-up">
+            <div class="blockquote">
+              <p>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making
+                this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined
+                with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.</p>
+            </div>
+            <h3 class="card-testimonial__name">Testimonial Name</h3>
+            <span class="card-testimonial__position">Testimonial position</span>
+          </div>
+        </div> <!-- //col-->
+        <div class="col">
+          <div class="card card-testimonial" data-aos="fade-up">
+            <div class="blockquote">
+              <p>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making
+                this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined
+                with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.</p>
+            </div>
+            <h3 class="card-testimonial__name">Testimonial Name</h3>
+            <span class="card-testimonial__position">Testimonial position</span>
+          </div>
+        </div> <!-- //col-->
+        <div class="col">
+          <div class="card card-testimonial" data-aos="fade-up">
+            <div class="blockquote">
+              <p>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making
+                this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined
+                with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.</p>
+            </div>
+            <h3 class="card-testimonial__name">Testimonial Name</h3>
+            <span class="card-testimonial__position">Testimonial position</span>
+          </div>
+        </div> <!-- //col-->
+        <div class="col">
+          <div class="card card-testimonial" data-aos="fade-up">
+            <div class="blockquote">
+              <p>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary,
+                making
+                this the first true generator on the Internet. It uses a dictionary of over 200 Latin words,
+                combined
+                with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.</p>
+            </div>
+            <h3 class="card-testimonial__name">Testimonial Name</h3>
+            <span class="card-testimonial__position">Testimonial position</span>
+          </div>
+        </div> <!-- //col-->
+      </div> <!-- //row -->
+    </div> <!-- //container-->
+  </section>
+</main><!-- #main -->
+
+<?php
+get_footer();
---
+++ page-quem-somos.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package academiadaodontologia
+ */
+
+get_header();
+?>
+
+<main id="main primary" class="site-main">
+  <section class="mt-5 py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="ui-title">
+            <h2 class="ui-title-main">Quem somos</h2>
+          </div>
+          <div class="text-light fs-5 fw-lighter content-entry">
+            <img class="float-start me-5 mb-5 mw-100"
+              src="<?php echo get_template_directory_uri(); ?>/assets/images/blog-image-1.jpg" alt="">
+            <p>A Academia da Odontologia foi criada em 2018 com o objetivo de oferecer uma plataforma completa e
+              acessível para quem almeja se manter atualizado com o que há de novo na Odontopediatria e Ortodontia.
+              Com
+              aulas online, a empresa tem a missão de entregar conteúdo de qualidade para que o aluno possa estudar
+              como
+              e onde quiser, dentro de sua disponibilidade.</p>
+            <p>Formada por uma equipe de especialistas em cursos preparatórios para a internet, a Academia da
+              Odontologia preza por atualidade em seus conteúdos e qualidade na produção. A equipe de professores
+              inclui
+              profissionais especializados em suas áreas de docência que trazem seus anos de experiência para as salas
+              de aula virtuais. Além de professores, muitos são também clínicos que conhecem por experiência própria o
+              dia a dia dos consultórios e a efetividade de seus tratamentos na prática.</p>
+            <p> O objetivo da Academia da Odontologia é, portanto, que qualquer pessoa possa estudar por um preço
+              justo
+              e dentro dos horários de que dispõe, bastando apenas ter um smartphone ou computador e uma conexão à
+              internet. Entendemos que qualquer profissional, em qualquer lugar do Brasil ou do mundo, deve ter a
+              oportunidade de estudar com excelentes professores e com um curso de didática prática e efetiva.</p>
+            <p>O objetivo da Academia da Odontologia é, portanto, que qualquer pessoa possa estudar por um preço justo
+              e
+              dentro dos horários de que dispõe, bastando apenas ter um smartphone ou computador e uma conexão à
+              internet. Entendemos que qualquer profissional, em qualquer lugar do Brasil ou do mundo, deve ter a
+              oportunidade de estudar com excelentes professores e com um curso de didática prática e efetiva.</p>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col mt-5">
+          <div class="ui-title">
+            <h2 class="ui-title-main">professores da academia</h2>
+          </div>
+        </div>
+      </div>
+      <div class="row mb-5">
+        <div class="col-12">
+          <div class="row text-light g-0  rounded overflow-hidden flex-md-row mb-5 shadow-sm position-relative">
+            <div class="col-lg-auto col-12 text-center d-lg-block mb-3">
+              <img width="112" class="border-0 rounded-circle" /
+                src="<?php echo get_template_directory_uri(); ?>assets/images/person-300x300.jpg" alt="">
+            </div>
+            <div class="col p-4 pt-1 ps-5 d-flex flex-column position-static">
+              <h3 class="fs-4 fw-bolder">Juliana Pereira Andriani</h3>
+              <div class="fw-lighter fs-5">
+                <p>
+                  Especialista em Odontopediatria - PUC/PR<br>
+                  Mestre em Odontologia / Área de Concentração Odontopediatria - UFSC/SC<br>
+                  Habilitação em Sedação Consciente com Óxido Nitroso<br>
+                  Professora e Coordenadora do Curso de Especialização e Atualização em Odontopediatria - ABCD<br>
+                  IAPD Membro do board 2019/2021 - Membership Committee<br>
+                  Idealizadora da CAIXA GUIA - Odontopediatria<br>
+                  Clinical Adviser NuSmile no Brasil
+                </p>
+              </div>
+              <ul class="list-inline fs-5">
+                <li class="list-inline-item">
+                  <a href="#" class="text-light"> <i class="fab fa-facebook-f"></i> </a>
+                </li>
+                <li class="list-inline-item ms-3">
+                  <a href="#" class="text-light"> <i class="fab fa-instagram"></i></a>
+                </li>
+                <li class="list-inline-item ms-3">
+                  <a href="#" class="text-light"> <i class="fab fa-youtube"></i></a>
+                </li>
+                <li class="list-inline-item ms-3">
+                  <a href="#" class="text-light"> <i class="fab fa-twitter"></i></a>
+                </li>
+                <li class="list-inline-item ms-3">
+                  <a href="#" class="text-light"> <i class="fab fa-linkedin"></i></a>
+                </li>
+              </ul>
+
+            </div>
+          </div>
+        </div> <!-- //col-->
+        <div class="col-12">
+          <div class="row text-light g-0  rounded overflow-hidden flex-md-row mb-5 shadow-sm position-relative">
+            <div class="col-lg-auto col-12 text-center d-lg-block mb-3">
+              <img width="112" class="border-0 rounded-circle"
+                src="<?php echo get_template_directory_uri(); ?>/assets/images/person-300x300.jpg" alt="">
+            </div>
+            <div class="col p-4 pt-1 ps-5 d-flex flex-column position-static">
+              <h3 class="fs-4 fw-bolder">Juliana Pereira Andriani</h3>
+              <div class="fw-lighter fs-5">
+                <p>
+                  Especialista em Odontopediatria - PUC/PR<br>
+                  Mestre em Odontologia / Área de Concentração Odontopediatria - UFSC/SC<br>
+                  Habilitação em Sedação Consciente com Óxido Nitroso<br>
+                  Professora e Coordenadora do Curso de Especialização e Atualização em Odontopediatria - ABCD<br>
+                  IAPD Membro do board 2019/2021 - Membership Committee<br>
+                  Idealizadora da CAIXA GUIA - Odontopediatria<br>
+                  Clinical Adviser NuSmile no Brasil
+                </p>
+              </div>
+              <ul class="list-inline fs-5">
+                <li class="list-inline-item">
+                  <a href="#" class="text-light"> <i class="fab fa-facebook-f"></i> </a>
+                </li>
+                <li class="list-inline-item ms-3">
+                  <a href="#" class="text-light"> <i class="fab fa-instagram"></i></a>
+                </li>
+                <li class="list-inline-item ms-3">
+                  <a href="#" class="text-light"> <i class="fab fa-youtube"></i></a>
+                </li>
+                <li class="list-inline-item ms-3">
+                  <a href="#" class="text-light"> <i class="fab fa-twitter"></i></a>
+                </li>
+                <li class="list-inline-item ms-3">
+                  <a href="#" class="text-light"> <i class="fab fa-linkedin"></i></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div> <!-- //col-->
+      </div>
+    </div>
+  </section>
+</main><!-- #main -->
+
+<?php
+get_footer();
